fda47c93256fa58616380feb59f93bd599fe3ec9
Jieun Bae <bzz1eun@gmail.com>
2016-09-18 21:54:02 +0900

    교수 컨트롤러와 프로그램 컨트롤러 연결

--- SchoolManagement/src/controllers/Controllers.java
+++ SchoolManagement/src/controllers/Controllers.java
@@ -1,5 +1,46 @@
 package controllers;
 
-public class Controllers {
+import professorControllers.ProfessorCourseController;
+import professorControllers.ProfessorMainController;
+import professorControllers.ProfessorNoticeController;
+import professorControllers.ProfessorScoreController;
 
+public class Controllers {
+	
+	private static ProgramController programController;
+	private static ProfessorCourseController professorCourseController;
+	private static ProfessorMainController professorMainController;
+	private static ProfessorNoticeController professorNoticeController;
+	private static ProfessorScoreController professorScoreController;	
+	
+	public Controllers() {
+		
+		professorCourseController = new ProfessorCourseController();
+		professorMainController = new ProfessorMainController();
+		professorNoticeController = new ProfessorNoticeController();
+		professorScoreController = new ProfessorScoreController();
+		
+	}
+
+	public static ProgramController getProgramController() {
+		return programController;
+	}
+
+	public static ProfessorCourseController getProfessorCourseController() {
+		return professorCourseController;
+	}
+
+	public static ProfessorMainController getProfessorMainController() {
+		return professorMainController;
+	}
+
+	public static ProfessorNoticeController getProfessorNoticeController() {
+		return professorNoticeController;
+	}
+
+	public static ProfessorScoreController getProfessorScoreController() {
+		return professorScoreController;
+	}
+	
+	
 }
