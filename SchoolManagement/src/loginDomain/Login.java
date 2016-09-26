dc798a342d5d065dcd4cad766c9297eb20e153d0
forsnbs <forsnbs@gmail.com>
2016-09-26 19:31:19 +0900

    [원태킴]A1

---
+++ SchoolManagement/src/loginDomain/Login.java
@@ -0,0 +1,34 @@
+package loginDomain;
+
+public class Login {
+
+	private String loginId;
+	private String loginPassword;
+	
+	public Login() {
+		
+	}
+
+	public Login(String loginId, String loginPassword) {
+		
+		this.loginId = loginId;
+		this.loginPassword = loginPassword;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+	
+}
