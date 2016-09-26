cca0bb5e0be9f21755816ace4237091f6e8bbfdf
slsrmsn <slsrmsn@gmail.com>
2016-09-26 17:27:30 +0900

    [남건우]B1

--- SchoolManagement/src/studentDAO/StudentMainDAO.java
+++ SchoolManagement/src/studentDAO/StudentMainDAO.java
@@ -1,6 +1,50 @@
 package studentDAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import controllers.Controllers;
+import studentDomain.Student;
+
 public class StudentMainDAO {
 
-	//
+	//학생정보조회
+	public ArrayList<Student> oneStudentList(int studentNumber) {
+
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Student> OntStudentList = new ArrayList<Student>();
+
+		try {
+			stmt = Controllers.getProgramController().getConnection().createStatement();
+			String sql = "select from where student_number = " + studentNumber;
+			rs = stmt.executeQuery(sql);
+
+			while(rs.next()) {
+				Student student = new Student();
+				student.setStudent_number(rs.getInt("student_number"));
+				student.setStudent_socialNumber(rs.getInt("student_socialNumber"));
+				student.setStudent_name(rs.getString("student_name"));
+				student.setStudent_password(rs.getString("student_password"));
+				student.setStudent_address(rs.getString("student_address"));
+				student.setStudent_email(rs.getString("student_email"));
+				student.setStudent_phoneNumber(rs.getString("student_phoneNumber"));	
+			}						
+		} catch (SQLException e) {
+			System.out.println("제품 목록 보기에서 예외 발생");
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try { rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+			}
+			if(stmt != null) {
+				try { stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
+			}
+		}
+
+		return OntStudentList;
+
+	}
 }
--- SchoolManagement/src/studentView/StudentMenuView.java
+++ SchoolManagement/src/studentView/StudentMenuView.java
@@ -1,5 +1,50 @@
 package studentView;
 
+import java.util.Scanner;
+
+import controllers.Controllers;
+import professorView.AlertView;
+
 public class StudentMenuView {
+	
+	private Scanner keyboard;
+	
+	public StudentMenuView() {
+
+		keyboard = new Scanner(System.in);
+
+	}
+	
+	//학생메인메뉴를 보여주는 뷰
+	private void menu(int StudentNumber) {
+
+		while(true) {
+			System.out.print("[1. 개인정보, 2. 신청정보, 3. 수강정보, 4. 성적정보, 5. 학사공지, 6. 로그아웃] : "); // 제품 수정
+			int selectedMenu = keyboard.nextInt();
+
+			if(selectedMenu == 1) {
+				new AlertView().alert("개인정보 컨트롤러에 개인 정보 보기를 요청함.");
+				//Controllers.getProductController().requestSelectList();
+			} else if(selectedMenu == 2) {
+				new AlertView().alert("신청정보 컨트롤러에 신청 정보 보기를 요청함.");
+				//Controllers.getCartController().requestInsert(productNumber);
+			} else if(selectedMenu == 3) {
+				new AlertView().alert("수강정보");
+				//Controllers.getProductController().requestUpdateProduct(productNumber);
+			} else if(selectedMenu == 4) {
+				new AlertView().alert("성적정보");
+				//Controllers.getProductController().requestDelete(productNumber);
+			} else if(selectedMenu == 5) {
+				new AlertView().alert("학사공지");
+				//Controllers.getProductController().requestDelete(productNumber);
+			} else if(selectedMenu == 6) {
+				new AlertView().alert("로그아웃");
+				//Controllers.getProductController().requestDelete(productNumber);
+			} else {
+				new AlertView().alert("메뉴를 다시 선택해 주세요.");
+			}
+		}
+
+	}
 
 }
