538a2282938ba6f8813253d746f5845df55f9dd4
forsnbs <forsnbs@gmail.com>
2016-09-30 20:54:04 +0900

    [김원태] 수연이 커밋 내용 포함 해서 다시 커밋

--- SchoolManagement/src/adminDomain/Subject.java
+++ SchoolManagement/src/adminDomain/Subject.java
@@ -1,7 +1,7 @@
 package adminDomain;
 
 public class Subject {
-	
+
 	private String subject_number; //과목 번호
 	private String subject_name; //과목 이름
 	private int subject_year; //과목 권장학년
@@ -10,12 +10,15 @@ public class Subject {
 	public Subject() {
 		
 	}
+
 	//과목 수정에 필요한 생성자
 	public Subject(String subject_name, int subject_year, int subject_grade) {
+	
 		
 		this.subject_name = subject_name;
 		this.subject_year = subject_year;
 		this.subject_grade = subject_grade;
+	
 		
 	}
 	//과목 등록에 필요한 생성자
@@ -28,36 +31,53 @@ public class Subject {
 		
 	}
 
+	
 	public String getSubject_number() {
 		return subject_number;
+	
 	}
 
 	public void setSubject_number(String subject_number) {
 		this.subject_number = subject_number;
+	
 	}
 
 	public String getSubject_name() {
+	
 		return subject_name;
+	
 	}
 
 	public void setSubject_name(String subject_name) {
+	
 		this.subject_name = subject_name;
+	
 	}
 
 	public int getSubject_year() {
+	
 		return subject_year;
+	
 	}
 
 	public void setSubject_year(int subject_year) {
+	
 		this.subject_year = subject_year;
+	
 	}
 
 	public int getSubject_grade() {
+	
 		return subject_grade;
+	
 	}
 
 	public void setSubject_grade(int subject_grade) {
+	
 		this.subject_grade = subject_grade;
+	
 	}
+	
+	
 	
 }
